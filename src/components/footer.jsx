24d2f26829a98e6c928b413470cf8cfb2ad69394
jenlu33 <nejul33@gmail.com>
2020-09-09 20:13:57 -0400

    Add footer info and font awesome icons

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
     <footer>
-      This is a footer
+      <div className="footer-contact">
+        <p>(540) 951-2380</p>
+      </div>
+      <div className="footer-address">
+        <p>801 University City Blvd, Ste. 15, Blacksburg, VA 24060</p>
+      </div>
+      <div className="social-handles">
+        {/* <p>Follow Us</p> */}
+        <a
+          className="footer-link"
+          href="https://www.facebook.com/sushifactoryandtea"
+        >
+          <FontAwesomeIcon icon={faFacebook} size="lg" />
+        </a>
+      </div>
     </footer>
-  )
+  );
 };
 
 export default Footer;
