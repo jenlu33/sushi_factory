8a13e6599ad295a43caf4624302291d2b2c1b346
jenlu33 <nejul33@gmail.com>
2020-09-30 11:33:36 -0700

    Add google analytics

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
+import { HashRouter, Route, Switch} from 'react-router-dom';
 import NavBar from './navbar';
 import Main from './main';
 import Menu from './menu/menu';
@@ -7,21 +9,35 @@ import Order from "./order";
 import Directions from './directions';
 import Footer from "./footer";
 
-const Root = () => (
-  <HashRouter>
-    <div className="main-div">
-      <NavBar />
-      
-      <Switch>
-        <Route path="/menu" component={Menu} />
-        <Route path="/order" component={Order} />
-        <Route path="/directions" component={Directions} />
-        <Route path="/" component={Main} />
-      </Switch>
 
-      <Footer />
-    </div>
+ReactGA.initialize("UA-179255435-1");
+// ReactGA.pageview("/homepage");
+
+
+const Root = () => {
+  const history = createBrowserHistory();
+  history.listen(() => {
+    console.log(history.location.hash);
+    ReactGA.set({ page: history.location.hash }); // Update the user's current page
+    ReactGA.pageview(history.location.hash); // Record a pageview for the given page
+  })
+
+  return (
+    <HashRouter>
+      <div className="main-div">
+        <NavBar />
+        
+        <Switch>
+          <Route path="/menu" component={Menu} />
+          <Route path="/order" component={Order} />
+          <Route path="/directions" component={Directions} />
+          <Route path="/" component={Main} />
+        </Switch>
+
+        <Footer />
+      </div>
   </HashRouter>
-)
+  )
+}
 
 export default Root;
