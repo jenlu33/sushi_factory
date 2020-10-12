d3ff49a2efc91a709d32d0e0e94a8edb8bf2a2e6
jenlu33 <nejul33@gmail.com>
2020-10-12 15:26:49 -0400

    Add scroll to top button on menu

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuSection from './menuSection';
 // import TeaPricing from './teaPricing';
 import TeaSection from './teaSection'
@@ -12,41 +12,68 @@ import icedTeaDodo from "../../data/icedTeaDodo";
 import specials from "../../data/specials";
 import teaExtras from '../../data/drinkExtras';
 
-function Menu() {
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 
-  return(
+function Menu() {
+  const [showScrollArrow, setScrollArrow] = useState(false);
+
+  const checkScroll = () => {
+    window.pageYOffset > 500 ? setScrollArrow(true) : setScrollArrow(false);
+  }
+
+  window.addEventListener("scroll", checkScroll);
+
+  const scrollToTop = () => {
+    window.scrollTo({top: 0, behavior: "smooth"});
+  };
+
+  return (
     <div className="main-menu-div">
       <div className="menu-container">
         <div className="menu-title-container">
           <h1 className="menu-title">Sushi</h1>
         </div>
-        <MenuSection 
-          menuType={sushiPlateMenu} title="Sushi Plates" customClass="plates"
-          imgURL={"01.jpg"}/>
-        <MenuSection menuType={sushiRollMenu} title="Sushi Rolls" customClass="rolls"/>
-        <MenuSection 
-          menuType={sushiSidesMenu} title="Sides" customClass="sides"
+        <MenuSection
+          menuType={sushiPlateMenu}
+          title="Sushi Plates"
+          customClass="plates"
+          imgURL={"01.jpg"}
+        />
+        <MenuSection
+          menuType={sushiRollMenu}
+          title="Sushi Rolls"
+          customClass="rolls"
+        />
+        <MenuSection
+          menuType={sushiSidesMenu}
+          title="Sides"
+          customClass="sides"
           imgURL={"05.jpg"}
-          />
+        />
       </div>
 
       <div className="menu-container">
         <div className="menu-title-container">
           <h1 className="menu-title">Tea</h1>
         </div>
-        <TeaSection menuType={milkTea} title="Bubble Tea"/>
-        <TeaSection menuType={icedTeaDodo} title="Shaken Iced Tea"/>
-        <TeaSection menuType={icedTeaDodo} title="Dodo Yogurt"/>
-        <SpecialsSection menuType={specials}/>
-        <MenuSection 
-          menuType={teaExtras} title="Extras" customClass="tea-extras"
-          imgURL={"06.jpg"}/>
+        <TeaSection menuType={milkTea} title="Bubble Tea" />
+        <TeaSection menuType={icedTeaDodo} title="Shaken Iced Tea" />
+        <TeaSection menuType={icedTeaDodo} title="Dodo Yogurt" />
+        <SpecialsSection menuType={specials} />
+        <MenuSection
+          menuType={teaExtras}
+          title="Extras"
+          customClass="tea-extras"
+          imgURL={"06.jpg"}
+        />
       </div>
-    
 
-      {/* Special Drinks Menu */}
+      <KeyboardArrowUpIcon 
+        onClick={scrollToTop} 
+        style={{ fontSize: "3rem", opacity: showScrollArrow ? "0.9" : "0", animation: showScrollArrow ? "fadeIn 0.3s" : "fadeOut 0.5s" }} 
+        className="scroll-up-btn" />
     </div>
-  )
+  );
 };
 
 export default Menu;
